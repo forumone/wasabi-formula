a08327e942e0e654697d81dc8f36c98b88e3ea28
Steve Huang <shuang@forumone.com>
2020-09-16 15:22:46 -0400

    double quoting and formatting

--- wasabi/wasabi-backup.sh
+++ wasabi/wasabi-backup.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/env bash
-set -e
+set -exuo pipefail
 
 # add aws secretsmanager commands to set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
 AWS_ACCESS_KEY_ID=$(aws secretsmanager get-secret-value --secret-id WASABI_ACCESS_KEY_ID --region us-east-1 | jq -r .SecretString) && export AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value --secret-id WASABI_SECRET_ACCESS_KEY --region us-east-1 | jq -r .SecretString) && export AWS_SECRET_ACCESS_KEY
 
-if [ -z ${AWS_ACCESS_KEY_ID} ] || [ -z ${AWS_SECRET_ACCESS_KEY} ]
+# exit 1 if access key not set
+if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ]
 then
     echo "awscli access key or secret not set, exiting"
     exit
@@ -32,20 +33,19 @@ function run {
        echo "Error with $1"
     fi
     return $status
-
 }
 
 touch ${lockfile}
 
 if [[ "$1" == "dumpdbs" ]]
 then
-  date=`date -I`
+  date=$(date -I)
   dir=/var/lib/mysqlbackups
-  databases=`/usr/bin/mysql -e 'show databases' -s --skip-column-names | /bin/grep -v information_schema | /bin/grep -v performance_schema`
+  databases=$(/usr/bin/mysql -e 'show databases' -s --skip-column-names | /bin/grep -v information_schema | /bin/grep -v performance_schema)
   file=$date.sql.gz
   test -d $dir || /bin/mkdir -p $dir
   echo "Dumping databases"
-  for i in $databases; do run /usr/bin/mysqldump --opt $i |gzip > $dir/$i.$file; done
+  for i in $databases; do run /usr/bin/mysqldump --opt "$i" |gzip > "$dir"/"$i"."$file"; done
   echo "Finished dumping databases"
   run /usr/bin/find $dir -ctime +7 -delete
 fi
@@ -53,11 +53,12 @@ fi
 # read paths from file, run aws sync against every valid path.
 while IFS= read -r line
 do
-source=$(realpath $line)
-if [ -d "$source" ] && [ -x "$source" ];
-then
-        echo "aws s3 sync $source s3://${bucket}${source} ${wasabi_cmd_suffix} | logger $(basename $0) $now $source backup SUCCESS" || logger $(basename $0) $source backup FAILED
-fi
+   source=$(realpath "$line")
+    if [ -d "$source" ] && [ -x "$source" ];
+    then
+        aws s3 sync --dryrun "$source" s3://"${bucket}${source}" "${wasabi_cmd_suffix}" | logger "$now" "$source" backup SUCCESS || logger "$source" backup FAILED
+    fi
 done < $input
 
+#cleanup
 rm -f ${lockfile}
