fc1b5dc794662cc2e2251e87bd0b9a2962fa0207
James Bernardi <jbernardi@forumone.com>
2021-06-01 13:14:56 -0400

    ofs snapshot mount and backup

--- wasabi/files/wasabi-daily.sh
+++ wasabi/files/wasabi-daily.sh
@@ -4,40 +4,23 @@ set -eo pipefail
 # exclude
 exclude="*>*"
 
-# file containing paths to backup, one per line.
-input="/etc/wasabi-backup.txt"
-
 # timestamp
 now=$(date +%F_%H-%M-%S)
 
 lockfile=/tmp/prebackup.lock
 
-function run {
-    "$@"
-    local status=$?
-    if [ $status -ne 0 ]; then
-       echo "Error with $1"
-    fi
-    return $status
-}
-
 touch ${lockfile}
-#Backup vhosts
-aws --profile wasabi  s3 sync /var/www/vhosts/ s3://"{{ wasabi_bucket }}/vhosts" --no-follow-symlinks --exclude "/etc/systemd/system/multi-user.target.wants/amazon-ssm-agent.service" --exclude "${exclude}" --endpoint-url=https://s3.wasabisys.com 2>&1 1>/dev/null && logger -t wasabi "$now" "$source" backup SUCCESS || logger -t wasabi "$source" backup ERROR
+#mount OFS Snapshot
+#get S3 FS
+snap=$(date +%Y-%m-%d)
+echo $snap
+ofs_snapshot=$(/sbin/mount.objectivefs list -sz james-ofs-q7fc1co4/www@$snap | tail -n 1 | awk '{print $1}')
+echo $ofs_snapshot
+/sbin/mount.objectivefs $ofs_snapshot /mnt/ofs_snapshot
 
-# read paths from file, run aws sync against every valid path.
-while IFS= read -r line
-do
-    if [[ ! -z ${line} ]];
-    then
-        source=$(realpath "$line")
-        if [ -d "$source" ] && [ -x "$source" ];
-        then
-            echo "[wasabi] ${now} beginning back up of ${source}..."
-            aws --profile wasabi  s3 sync /var/www/vhosts s3://"{{ wasabi_bucket }}/vhosts" --no-follow-symlinks --exclude "/etc/systemd/system/multi-user.target.wants/amazon-ssm-agent.service" --exclude "${exclude}" --endpoint-url=https://s3.wasabisys.com 2>&1 1>/dev/null && logger -t wasabi "$now" "$source" backup SUCCESS || logger -t wasabi "$source" backup ERROR
-        fi
-    fi
-done < $input
+#Backup Snapshot
+aws --profile wasabi  s3 sync /mnt/ofs_snapshot/ s3://"{{ wasabi_bucket }}/vhosts" --no-follow-symlinks --exclude "/etc/systemd/system/multi-user.target.wants/amazon-ssm-agent.service" --exclude "${exclude}" --endpoint-url=https://s3.wasabisys.com 2>&1 1>/dev/null && logger -t wasabi "$now" "$source" backup SUCCESS || logger -t wasabi "$source" backup ERROR
 
 #cleanup
+umount /mnt/ofs_snapshot
 rm -f ${lockfile}
