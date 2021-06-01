d4dd24b02ab73c771288f9a39abc4bd8d321887f
James Bernardi <jbernardi@forumone.com>
2021-06-01 13:39:12 -0400

    debug cleanup

--- wasabi/files/wasabi-daily.sh
+++ wasabi/files/wasabi-daily.sh
@@ -9,17 +9,24 @@ now=$(date +%F_%H-%M-%S)
 
 lockfile=/tmp/prebackup.lock
 
+function run {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+       echo "Error with $1"
+    fi
+    return $status
+}
+
 touch ${lockfile}
 #mount OFS Snapshot
 #get S3 FS
 snap=$(date +%Y-%m-%d)
-echo $snap
 ofs_snapshot=$(/sbin/mount.objectivefs list -sz james-ofs-q7fc1co4/www@$snap | tail -n 1 | awk '{print $1}')
-echo $ofs_snapshot
 /sbin/mount.objectivefs $ofs_snapshot /mnt/ofs_snapshot
 
 #Backup Snapshot
-aws --profile wasabi  s3 sync /mnt/ofs_snapshot/ s3://"{{ wasabi_bucket }}/vhosts" --no-follow-symlinks --exclude "/etc/systemd/system/multi-user.target.wants/amazon-ssm-agent.service" --exclude "${exclude}" --endpoint-url=https://s3.wasabisys.com 2>&1 1>/dev/null && logger -t wasabi "$now" "$source" backup SUCCESS || logger -t wasabi "$source" backup ERROR
+run aws --profile wasabi  s3 sync /mnt/ofs_snapshot/ s3://"{{ wasabi_bucket }}/" --no-follow-symlinks --exclude "/etc/systemd/system/multi-user.target.wants/amazon-ssm-agent.service" --exclude "${exclude}" --endpoint-url=https://s3.wasabisys.com 2>&1 1>/dev/null && logger -t wasabi "$now" "$source" backup SUCCESS || logger -t wasabi "$source" backup ERROR
 
 #cleanup
 umount /mnt/ofs_snapshot
