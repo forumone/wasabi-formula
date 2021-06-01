947716e961d058db2a01591f6b593919f77fe9ce
James Bernardi <jbernardi@forumone.com>
2021-06-01 14:41:24 -0400

    update weekly

--- wasabi/files/wasabi-weekly.sh
+++ wasabi/files/wasabi-weekly.sh
@@ -1,18 +1,43 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#! /usr/bin/env bash
+set -eo pipefail
 
-wasabi_cmd_suffix="
+# timestamp
+now=$(date +%F_%H-%M-%S)
 
-bucket=$WASABI_BUCKET
-target="/var/www/vhosts"
-timestamp=$(date +%F-%H%M)
+lockfile=/tmp/prebackup.lock
 
-if cd ${target}
+function run {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+       echo "Error with $1"
+    fi
+    return $status
+}
+
+#mount OFS Snapshot
+#get S3 FS
+snap=$(date +%Y-%m-%d)
+ofs_snapshot=$(/sbin/mount.objectivefs list -sz james-ofs-q7fc1co4/www@$snap | tail -n 1 | awk '{print $1}')
+if [ $snap -z ]
 then
-    for i in *
-    do
-        logger -t wasabi backup of ${target} beginning at ${timestamp}
-        tar -czf - $i | aws --profile wasabi s3 cp - s3://${bucket}${target}-weekly/${i}-${timestamp}.tar.gz --endpoint-url=https://s3.wasabisys.com
+echo "Unable to get latest OFS Snapshot"
+exit 1
+elif [ test -f "/mnt/ofs_snapshot/README" ]
+echo "Unable to mount snapshot!"
+exit 1
+else
+/sbin/mount.objectivefs $ofs_snapshot /mnt/ofs_snapshot
+fi
+
+if [test -f "/mnt/ofs_snapshot/README" ]
+then
+touch $lockfile
+#Back up folders to tar.gz format
+for i in $(ls /mnt/ofs_snapshot/vhosts/)
+  do
+        logger -t wasabi tar backup of vhosts beginning at ${timestamp}
+        tar -czf - $i | aws --profile wasabi s3 cp - s3://${bucket}/vhosts-weekly/${i}-${timestamp}.tar.gz --endpoint-url=https://s3.wasabisys.com
         if [ $? ]
         then
             logger -t wasabi ${target}/$i backup success up at ${timestamp}
@@ -21,3 +46,12 @@ then
         fi
     done
 fi
+elif
+echo "Objective FS Snapshot is not mounted, Unable to backup"
+exit 1
+fi
+
+#cleanup
+umount /mnt/ofs_snapshot
+rm -f ${lockfile}
+
