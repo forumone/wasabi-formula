8b0bfa78cb013a8bcac549cd2ae8bcd2956a07ff
James Bernardi <jbernardi@forumone.com>
2021-06-01 14:54:40 -0400

    this

--- wasabi/files/wasabi-daily.sh
+++ wasabi/files/wasabi-daily.sh
@@ -24,24 +24,24 @@ snap=$(date +%Y-%m-%d)
 ofs_snapshot=$(/sbin/mount.objectivefs list -sz james-ofs-q7fc1co4/www@$snap | tail -n 1 | awk '{print $1}')
 if [ $ofs_snapshot -z ]
 then
-echo "Unable to get latest OFS Snapshot"
-exit 1
+  echo "Unable to get latest OFS Snapshot"
+  exit 1
 elif [ test -f "/mnt/ofs_snapshot/README" ]
 then
-echo "Unable to mount snapshot!"
-exit 1
+  echo "Unable to mount snapshot!"
+  exit 1
 else
-/sbin/mount.objectivefs $ofs_snapshot /mnt/ofs_snapshot
+  /sbin/mount.objectivefs $ofs_snapshot /mnt/ofs_snapshot
 fi
 
 if [test -f "/mnt/ofs_snapshot/README" ]
 then
-touch $lockfile
+  touch $lockfile
 #Back up Snapshot
-run aws --profile wasabi  s3 sync /mnt/ofs_snapshot/ s3://"{{ wasabi_bucket }}/" --no-follow-symlinks --exclude "/etc/systemd/system/multi-user.target.wants/amazon-ssm-agent.service" --exclude "${exclude}" --endpoint-url=https://s3.wasabisys.com 2>&1 1>/dev/null && logger -t wasabi "$now" "$source" backup SUCCESS || logger -t wasabi "$source" backup ERROR
+  run aws --profile wasabi  s3 sync /mnt/ofs_snapshot/ s3://"{{ wasabi_bucket }}/" --no-follow-symlinks --exclude "/etc/systemd/system/multi-user.target.wants/amazon-ssm-agent.service" --exclude "${exclude}" --endpoint-url=https://s3.wasabisys.com 2>&1 1>/dev/null && logger -t wasabi "$now" "$source" backup SUCCESS || logger -t wasabi "$source" backup ERROR
 else
-echo "Objective FS Snapshot is not mounted, Unable to backup"
-exit 1
+  echo "Objective FS Snapshot is not mounted, Unable to backup"
+  exit 1
 fi
 
 #cleanup
