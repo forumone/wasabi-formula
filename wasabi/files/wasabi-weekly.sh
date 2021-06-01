7f2d3f3e61b552c37e323c8999e8f5275f44df17
James Bernardi <jbernardi@forumone.com>
2021-06-01 19:32:46 -0400

    don't backup healthcheck

--- wasabi/files/wasabi-weekly.sh
+++ wasabi/files/wasabi-weekly.sh
@@ -18,32 +18,30 @@ function run {
 #get S3 FS
 snap=$(date +%Y-%m-%d)
 ofs_snapshot=$(/sbin/mount.objectivefs list -sz james-ofs-q7fc1co4/www@$snap | tail -n 1 | awk '{print $1}')
-if test -z $snap
-then
+if test -z $snap; then
 echo "Unable to get latest OFS Snapshot"
 exit 1
-elif test -f "/mnt/ofs_snapshot/README"
-then
+elif test -f "/mnt/ofs_snapshot/README"; then
 echo "Unable to mount snapshot!"
 exit 1
 else
 /sbin/mount.objectivefs $ofs_snapshot /mnt/ofs_snapshot
 fi
 
-if test -f "/mnt/ofs_snapshot/README"
-then
+if test -f "/mnt/ofs_snapshot/README"; then
 touch $lockfile
 #Back up folders to tar.gz format
 for i in $(ls /mnt/ofs_snapshot/vhosts/)
   do
+  if [[ "$i" != "healtcheck" ]]; then
         logger -t wasabi tar backup of vhosts beginning at ${timestamp}
-        tar -czf - $i | aws --profile wasabi s3 cp - s3://{{ wasabi_bucket }}/vhosts-weekly/${i}-${timestamp}.tar.gz --endpoint-url=https://s3.wasabisys.com
-        if [ $? ]
-        then
-            logger -t wasabi ${target}/$i backup success up at ${timestamp}
+        tar czfP - /mnt/ofs_snapshot/vhosts/$i | aws --profile wasabi s3 cp - s3://{{ wasabi_bucket }}/vhosts-weekly/${i}-${timestamp}.tar.gz --endpoint-url=https://s3.wasabisys.com
+        if [ $? ]; then
+            logger -t wasabi $i backup success up at ${timestamp}
         else
-            logger -t wasabi ${target}/$i error at ${timestamp}
+            logger -t wasabi $i error at ${timestamp}
         fi
+  fi
     done
 else
 echo "Objective FS Snapshot is not mounted, Unable to backup"
