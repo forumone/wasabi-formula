31a6567270f7f50ea76139ef66a3a4ce73ce6e63
James Bernardi <jbernardi@forumone.com>
2021-06-01 14:49:00 -0400

    fix that var

--- wasabi/files/wasabi-daily.sh
+++ wasabi/files/wasabi-daily.sh
@@ -22,7 +22,7 @@ function run {
 #get S3 FS
 snap=$(date +%Y-%m-%d)
 ofs_snapshot=$(/sbin/mount.objectivefs list -sz james-ofs-q7fc1co4/www@$snap | tail -n 1 | awk '{print $1}')
-if [ $snap -z ]
+if [ $ofs_snapshot -z ]
 then
 echo "Unable to get latest OFS Snapshot"
 exit 1
