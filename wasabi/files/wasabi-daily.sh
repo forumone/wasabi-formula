d5368fa63935cf6ebd7a5d908f1909f783a78bfe
James Bernardi <jbernardi@forumone.com>
2021-06-01 14:16:33 -0400

    add some logic

--- wasabi/files/wasabi-daily.sh
+++ wasabi/files/wasabi-daily.sh
@@ -18,15 +18,30 @@ function run {
     return $status
 }
 
-touch ${lockfile}
 #mount OFS Snapshot
 #get S3 FS
 snap=$(date +%Y-%m-%d)
 ofs_snapshot=$(/sbin/mount.objectivefs list -sz james-ofs-q7fc1co4/www@$snap | tail -n 1 | awk '{print $1}')
+if [ $snap -z ]
+then
+echo "Unable to get latest OFS Snapshot"
+exit 1
+elif [ test -f "/mnt/ofs_snapshot/README" ]
+echo "Unable to mount snapshot!"
+exit 1
+else
 /sbin/mount.objectivefs $ofs_snapshot /mnt/ofs_snapshot
+fi
 
-#Backup Snapshot
+if [test -f "/mnt/ofs_snapshot/README" ]
+then
+touch $lockfile
+#Back up Snapshot
 run aws --profile wasabi  s3 sync /mnt/ofs_snapshot/ s3://"{{ wasabi_bucket }}/" --no-follow-symlinks --exclude "/etc/systemd/system/multi-user.target.wants/amazon-ssm-agent.service" --exclude "${exclude}" --endpoint-url=https://s3.wasabisys.com 2>&1 1>/dev/null && logger -t wasabi "$now" "$source" backup SUCCESS || logger -t wasabi "$source" backup ERROR
+elif
+echo "Objective FS Snapshot is not mounted, Unable to backup"
+exit 1
+fi
 
 #cleanup
 umount /mnt/ofs_snapshot
